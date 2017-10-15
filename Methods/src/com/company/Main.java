a6aae2065ddf2dee97eef684c4f2116c1d5ad03c
Yahya Hussein <yahyahussein91@gmail.com>
2017-10-14 21:17:57 -0700

    pasted method code from previous section

--- Methods/src/com/company/Main.java
+++ Methods/src/com/company/Main.java
@@ -4,5 +4,28 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if(gameOver){
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Your final score was " + finalScore);
+        }
+
+        score = 1000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        if(gameOver){
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
     }
+
+    public static void calculateScore(){
+
+        }
 }
