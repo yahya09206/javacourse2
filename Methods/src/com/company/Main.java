40951c6fa216258c3849590eff7782667e201ae6
Yahya Hussein <yahyahussein91@gmail.com>
2017-10-14 21:35:44 -0700

    refactored/commented out old code

--- Methods/src/com/company/Main.java
+++ Methods/src/com/company/Main.java
@@ -10,22 +10,23 @@ public class Main {
         int bonus = 100;
 
         calculateScore(true, 900, 5, 100);
+        calculateScore(true, 10000, 8, 200);
 
-        score = 10000;
-        levelCompleted = 8;
-        bonus = 200;
-
-        if(gameOver){
-            int finalScore = score + (levelCompleted * bonus);
-            System.out.println("Your final score was " + finalScore);
-        }
+//        score = 10000;
+//        levelCompleted = 8;
+//        bonus = 200;
+//
+//        if(gameOver){
+//            int finalScore = score + (levelCompleted * bonus);
+//            System.out.println("Your final score was " + finalScore);
+//        }
     }
 
     public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
+//        boolean gameOver = true;
+//        int score = 800;
+//        int levelCompleted = 5;
+//        int bonus = 100;
 
         if(gameOver){
             int finalScore = score + (levelCompleted * bonus);
