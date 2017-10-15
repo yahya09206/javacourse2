72b508dd21920fc92854f0a607a54bcd3efa75b4
Yahya Hussein <yahyahussein91@gmail.com>
2017-10-14 21:56:43 -0700

    added 2nd method

--- Methods/src/com/company/Main.java
+++ Methods/src/com/company/Main.java
@@ -40,8 +40,20 @@ public class Main {
         }
     }
 
-    public static int displayHighScorePosition(){
+    public static void displayHighScorePosition(String playerName, int playerPosition){
+        System.out.println(playerName + " managed to get into position " + playerPosition + " on the highscore table");
+    }
 
+    public static int calculateHighScorePosition(int playerScore){
+        if(playerScore > 1000){
+            return 1;
+        }else if(playerScore > 500 <= 1000){
+            return 2;
+        }else if(playerScore > 100 <= 500){
+            return 3;
+        }else{
+            return 4;
+        }
     }
 }
 
