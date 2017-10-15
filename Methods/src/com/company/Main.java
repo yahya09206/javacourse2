9a7d1c131704ba5a60f7e5876d5bccdd07df46b7
Yahya Hussein <yahyahussein91@gmail.com>
2017-10-14 21:24:13 -0700

    added comments

--- Methods/src/com/company/Main.java
+++ Methods/src/com/company/Main.java
@@ -15,7 +15,7 @@ public class Main {
             System.out.println("Your final score was " + finalScore);
         }
 
-        score = 1000;
+        score = 10000;
         levelCompleted = 8;
         bonus = 200;
 
@@ -29,3 +29,13 @@ public class Main {
 
         }
 }
+
+
+
+//Methods are created once but are able to be used over and over again
+//methods start outside of other methods but inside of public class
+//arguments are the actual values we want the variables to store and use
+//passing parameters is the same as declaring variables for them
+//void means don't return anything back
+//if you want to return something then replace void with datatype that you want to return
+//needs something returned even if game isn't played
