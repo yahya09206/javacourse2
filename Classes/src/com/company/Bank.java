fc014c433821fea101184cf483f74ef58d86c584
Yahya Hussein <yahyahussein91@gmail.com>
2017-10-18 22:18:29 -0700

    created getters and setters for fields

--- Classes/src/com/company/Bank.java
+++ Classes/src/com/company/Bank.java
@@ -7,4 +7,44 @@ public class Bank {
     private String customerName;
     private String email;
     private String phoneNumber;
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
