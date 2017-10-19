83fcc914f9dbde2f63bc21084060837938162aa6
Yahya Hussein <yahyahussein91@gmail.com>
2017-10-18 21:41:02 -0700

    updated notes

--- Classes/src/com/company/Main.java
+++ Classes/src/com/company/Main.java
@@ -6,3 +6,9 @@ public class Main {
 	// write your code here
     }
 }
+
+/*
+-classes are real world objects that consist of state and behavior
+-software objects store states in fields and behaviors in methods
+-class is a template for creating objects
+*/
