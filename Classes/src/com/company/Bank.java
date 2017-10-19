4d7803ea3e34682061d2c58d05cd5e217df2cd0c
Yahya Hussein <yahyahussein91@gmail.com>
2017-10-18 22:17:39 -0700

    created private fields for challenge

---
+++ Classes/src/com/company/Bank.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Bank {
+    //private fields
+    private int accountNumber;
+    private int balance;
+    private String customerName;
+    private String email;
+    private String phoneNumber;
+}
