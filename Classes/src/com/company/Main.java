fb45c15bb438f0697e23fb34c958e6a309b31568
Yahya Hussein <yahyahussein91@gmail.com>
2017-10-18 21:54:29 -0700

    defined model type for car

--- Classes/src/com/company/Main.java
+++ Classes/src/com/company/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Car audi = new Car();
+        Car bmw = new Car();
+        audi.model = "A8";
     }
 }
 
@@ -14,5 +16,5 @@ public class Main {
 -class is a template for creating objects
 -when defining field in java you go with the private modifiers
 -encapsulation hides fields from view outside of objects definition
-
+-
 */
