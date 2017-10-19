f22888b1e5ed808e2c85d4ba95283d2ee1bf9da0
Yahya Hussein <yahyahussein91@gmail.com>
2017-10-18 17:27:27 -0700

    created new method

--- MethodOverLoading/src/com/company/Main.java
+++ MethodOverLoading/src/com/company/Main.java
@@ -4,5 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+    }
+
+    public static int calculateScore(String playerName, int score){
+        System.out.println("Player " + playerName + " scored " + score + " points" );
+        return score * 1000;
     }
 }
+
+//Lets you use the same methods with different parameters
