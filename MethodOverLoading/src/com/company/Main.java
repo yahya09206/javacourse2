1b3edb05b0b713fa863b0857dd72c30539debd11
Yahya Hussein <yahyahussein91@gmail.com>
2017-10-18 19:14:12 -0700

    starting on conversion challenge

--- MethodOverLoading/src/com/company/Main.java
+++ MethodOverLoading/src/com/company/Main.java
@@ -4,27 +4,29 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int newScore = calculateScore("john", 500);
-        System.out.println("New score " + newScore);
-        calculateScore(75);
-        calculateScore();
+
+
+//        int newScore = calculateScore("john", 500);
+//        System.out.println("New score " + newScore);
+//        calculateScore(75);
+//        calculateScore();
 
     }
 
-    public static int calculateScore(String playerName, int score){
-        System.out.println("Player " + playerName + " scored " + score + " points" );
-        return score * 1000;
-    }
-
-    public static int calculateScore(int score){
-        System.out.println("Unmamed player scored " + score + " points" );
-        return score * 1000;
-    }
-
-    public static int calculateScore(){
-        System.out.println("No player name with 0 points" );
-        return 0;
-    }
+//    public static int calculateScore(String playerName, int score){
+//        System.out.println("Player " + playerName + " scored " + score + " points" );
+//        return score * 1000;
+//    }
+//
+//    public static int calculateScore(int score){
+//        System.out.println("Unmamed player scored " + score + " points" );
+//        return score * 1000;
+//    }
+//
+//    public static int calculateScore(){
+//        System.out.println("No player name with 0 points" );
+//        return 0;
+//    }
 
 }
 
