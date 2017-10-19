0e66208933c84fb5c2ff0d8d3e909f4d738de63d
Yahya Hussein <yahyahussein91@gmail.com>
2017-10-18 21:58:23 -0700

    created setter for model field

--- Classes/src/com/company/Car.java
+++ Classes/src/com/company/Car.java
@@ -4,7 +4,13 @@ public class Car {
 
     private int doors;
     private int wheels;
-    public String model;
+    private String model;
     private String engine;
     private String colour;
+
+    //method for class that's attached to class publicly accessible
+    public void setModel(String model){
+        //keyword "this" refers to the field you're creating the setter and getter for
+        this.model = model;
+    }
 }
