25e907b4217650d79ca54b9a0583b9cd263dc421
Yahya Hussein <yahyahussein91@gmail.com>
2017-10-18 17:29:48 -0700

    added new value for points

--- MethodOverLoading/src/com/company/Main.java
+++ MethodOverLoading/src/com/company/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int newScore = calculateScore("john", 500);
+        System.out.println("New score " + newScore);
 
     }
 
