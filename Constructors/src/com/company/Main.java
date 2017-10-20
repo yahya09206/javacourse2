32cb3251415438accf135972cc746bc5aa34ea1f
Yahya Hussein <yahyahussein91@gmail.com>
2017-10-19 21:00:37 -0700

    constructor runs with params called

--- Constructors/src/com/company/Main.java
+++ Constructors/src/com/company/Main.java
@@ -8,6 +8,11 @@ public class Main {
         Bank bobsAccount = new Bank(); //(12345, 123.45, "Yahya", "123@123.com", "1234567890");
         System.out.println(bobsAccount.getAccountNumber());
         System.out.println(bobsAccount.getBalance());
+
+        //calling constructor with default values that were generated
+        Bank timsAccount = new Bank("mike", "mke@gm.com", "23454");
+        System.out.println(timsAccount.getPhoneNumber() + " name " + timsAccount.getCustomerName());
+
     }
 }
 //Constructors let you set values easier
