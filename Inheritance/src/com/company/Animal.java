b5cee3d0c1592f70db793c753b9b92073f21e54b
Yahya Hussein <yahyahussein91@gmail.com>
2017-10-19 21:36:46 -0700

    created fields for animal

---
+++ Inheritance/src/com/company/Animal.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Animal {
+    //fields for animal
+    private String name;
+    private int brain;
+    private int body;
+    private int size;
+    private int weight;
+}
