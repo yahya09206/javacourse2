3cea73d05e7de0b4c2fca7521dc14fab9deb6160
Yahya Hussein <yahyahussein91@gmail.com>
2017-10-19 21:39:19 -0700

    generated constructor for dog class

--- Inheritance/src/com/company/Dog.java
+++ Inheritance/src/com/company/Dog.java
@@ -1,4 +1,11 @@
 package com.company;
 
-public class Dog {
+public class Dog extends Animal {
+    //Call constructor from other class by generating constructor
+    public Dog(String name, int brain, int body, int size, int weight) {
+        super(name, brain, body, size, weight);
+    }
 }
+
+
+//Extends keywords lets you inherits from another one
