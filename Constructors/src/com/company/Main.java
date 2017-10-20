a639f2ca8e9c8c1d2dffbf4bbaeeb57cd44a35d9
Yahya Hussein <yahyahussein91@gmail.com>
2017-10-19 20:39:59 -0700

    fixed error where constructor wasnt being called

--- Constructors/src/com/company/Main.java
+++ Constructors/src/com/company/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        //new instance of bank class
+        Bank bobsAccount = new Bank();
     }
 }
 //Constructors let you set values easier
