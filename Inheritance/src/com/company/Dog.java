843540b1386fe1df36b43f433c21e85500d5a4ba
Yahya Hussein <yahyahussein91@gmail.com>
2017-10-19 21:51:40 -0700

    new instance of dog

--- Inheritance/src/com/company/Dog.java
+++ Inheritance/src/com/company/Dog.java
@@ -12,7 +12,7 @@ public class Dog extends Animal {
     public Dog(String name, int size, int weight, int eyes, int legs, int tail, int teeth, String coat) {
         super(name, 1 , 1, size, weight);
         this.eyes = eyes;
-        this.legs - legs;
+        this.legs = legs;
         this.tail = tail;
         this.teeth = teeth;
         this.coat = coat;
--- Inheritance/src/com/company/Main.java
+++ Inheritance/src/com/company/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        //create animal
+        Animal animal = new Animal("animal", 1,1,5,5);
+        Dog dog = new Dog("Yorkie", 8, 20, 2, 4, 1, 20, "long silky");
     }
 }
