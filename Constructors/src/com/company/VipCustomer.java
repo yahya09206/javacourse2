aed893f48719abbe2fc0592e52066bd4dfdddc6c
Yahya Hussein <yahyahussein91@gmail.com>
2017-10-19 21:13:16 -0700

    generated getters

--- Constructors/src/com/company/VipCustomer.java
+++ Constructors/src/com/company/VipCustomer.java
@@ -5,4 +5,21 @@ public class VipCustomer {
     private double creditLimit;
     private String name;
     private String emailAddress;
+
+    public VipCustomer(){
+        this(123.45,"mike","mike@123.com");
+        System.out.println("Empty constructor called");
+    }
+
+    public double getCreditLimit() {
+        return creditLimit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
 }
