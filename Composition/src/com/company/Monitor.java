e79b45cbd68248c4282b7bf3cec9633c3fb7e0b3
Yahya Hussein <yahyahussein91@gmail.com>
2017-10-27 19:50:11 -0700

    created resolution class and fields

--- Composition/src/com/company/Monitor.java
+++ Composition/src/com/company/Monitor.java
@@ -16,4 +16,20 @@ public class Monitor {
     public void drawPixelAt(int x, int y, String color){
         System.out.println("Drawing pixel at " + x + "," + y + " in colour " + color);
     }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public Resolution getNativeResolution() {
+        return nativeResolution;
+    }
 }
