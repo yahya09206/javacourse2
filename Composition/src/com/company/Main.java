b640f30ed64d89dff5e3b6f5f37ca39a5e304379
Yahya Hussein <yahyahussein91@gmail.com>
2017-10-27 20:08:00 -0700

    ceating pc

--- Composition/src/com/company/Main.java
+++ Composition/src/com/company/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Dimensions dimensions = new Dimensions(20,20,5);
+        Case theCase = new Case("2208", "Apple", "240", dimensions);
+
+        Monitor theMonitor = new Monitor("27inch iPro", "iMac", 27, new Resolution(2540, 1440));
     }
 }
 
