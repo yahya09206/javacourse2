6bddeef7afed088aecf5929044bcb6f7c1b23b8d
Yahya Hussein <yahyahussein91@gmail.com>
2017-10-27 20:16:28 -0700

    set up motherboard

--- Composition/src/com/company/Main.java
+++ Composition/src/com/company/Main.java
@@ -4,10 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        //Case for iMac
         Dimensions dimensions = new Dimensions(20,20,5);
         Case theCase = new Case("2208", "Apple", "240", dimensions);
 
+        //Monitors
         Monitor theMonitor = new Monitor("27inch iPro", "iMac", 27, new Resolution(2540, 1440));
+
+        //Motherboard
+        Motherboard theMotherBoard = new Motherboard("BJ-200", "Apple", 4, 5, "v2.44");
     }
 }
 
