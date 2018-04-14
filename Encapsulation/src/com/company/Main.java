25697c35b9c25babb6a31d7c5dff456971dcebd3
Yahya Hussein <yahyahussein91@gmail.com>
2018-04-13 18:59:12 -0700

    created player class

--- Encapsulation/src/com/company/Main.java
+++ Encapsulation/src/com/company/Main.java
@@ -4,5 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int lives = 3;
+        boolean isGameOver = (lives < 1);
+        if(isGameOver){
+            System.out.println("Game over!");
+        }else {
+            System.out.println("You're still alive");
+        }
     }
 }
