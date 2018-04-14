2e7e8d79c1a153a8108f70854590c3be95d00a3a
Yahya Hussein <yahyahussein91@gmail.com>
2018-04-13 19:04:17 -0700

    set up fields

--- Encapsulation/src/com/company/Player.java
+++ Encapsulation/src/com/company/Player.java
@@ -1,4 +1,15 @@
 package com.company;
 
 public class Player {
+    String handleName;
+    int lives;
+    int level;
+    int score;
+
+    public Player(){
+        handleName = "Unknown player";
+        lives = 3;
+        level = 1;
+        score = 0;
+    }
 }
