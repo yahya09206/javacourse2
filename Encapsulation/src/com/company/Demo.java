210fb6df0051f33bf7cbeae35c0cc724423eca65
Yahya Hussein <yahyahussein91@gmail.com>
2018-04-13 19:12:55 -0700

    made fields private

--- Encapsulation/src/com/company/Demo.java
+++ Encapsulation/src/com/company/Demo.java
@@ -4,5 +4,7 @@ public class Demo {
 
     public static void main(String[] args) {
 
+        Player tim = new Player();
+        System.out.println(tim.getHandleName());
     }
 }
--- Encapsulation/src/com/company/Player.java
+++ Encapsulation/src/com/company/Player.java
@@ -1,15 +1,19 @@
 package com.company;
 
 public class Player {
-    String handleName;
-    int lives;
-    int level;
-    int score;
+    private String handleName;
+    private int lives;
+    private int level;
+    private int score;
 
     public Player(){
         handleName = "Unknown player";
         lives = 3;
         level = 1;
         score = 0;
+    }
+
+    public String getHandleName(){
+        return handleName;
     }
 }
