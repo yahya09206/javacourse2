47bb181a48355b2d13b2128364bf9c0b8047e4ff
Yahya Hussein <yahyahussein91@gmail.com>
2018-04-16 15:29:13 -0700

    set up basic loot class

--- Encapsulation/src/com/company/Loot.java
+++ Encapsulation/src/com/company/Loot.java
@@ -12,4 +12,28 @@ public class Loot {
         this.type = type;
         this.value = value;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LootType getType() {
+        return type;
+    }
+
+    public void setType(LootType type) {
+        this.type = type;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
 }
