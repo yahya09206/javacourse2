fc3b0bf5f03e2f007b5d73fe0411c4678ee3680d
Yahya Hussein <yahyahussein91@gmail.com>
2018-04-16 16:03:45 -0700

    new enemy class

--- Encapsulation/src/com/company/Demo.java
+++ Encapsulation/src/com/company/Demo.java
@@ -6,32 +6,6 @@ public class Demo {
 
     public static void main(String[] args) {
 
-        Player tim = new Player("Tom Smith");
-        System.out.println(tim.getHandleName());
-        System.out.println("Level: " + tim.getLevel());
-        System.out.println("Lives: " + tim.getLives());
-//        System.out.println(tim.getWeapon().getName());
-//        Weapon timsWeapon = tim.getWeapon();
-//        System.out.println(timsWeapon.getName());
-        System.out.println(tim.getWeapon().getName());
-
-        Weapon myAxe = new Weapon("Goldshine Axe", 50, 50);
-        tim.setWeapon(myAxe);
-        System.out.println(tim.getWeapon().getName());
-
-        Loot redPotion = new Loot("Red Potion", LootType.POTION, 7);
-        tim.pickUpLoot(redPotion);
-
-        tim.pickUpLoot(new Loot("+3 chest armor", LootType.ARMOR, 80));
-        tim.pickUpLoot(new Loot("+2 ring of protection", LootType.RING, 40));
-        tim.pickUpLoot(new Loot("Invisibility Potion", LootType.POTION, 35));
-
-        tim.showInventory();
-
-//        Loot bluePotion = new Loot("Blue Potion", LootType.POTION, 6);
-        boolean wasDeleted = tim.dropLoot(redPotion);
-        System.out.println(wasDeleted);
-        tim.showInventory();
 
     }
 }
