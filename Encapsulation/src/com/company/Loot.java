14761fb8e4e03772b4d17a537435c6b9767da7af
Yahya Hussein <yahyahussein91@gmail.com>
2018-04-16 15:28:50 -0700

    created constructor

--- Encapsulation/src/com/company/Loot.java
+++ Encapsulation/src/com/company/Loot.java
@@ -3,5 +3,13 @@ package com.company;
 enum LootType { POTION, RING, ARMOR }
 
 public class Loot {
+    private String name;
+    private LootType type;
+    private int value;
 
+    public Loot(String name, LootType type, int value) {
+        this.name = name;
+        this.type = type;
+        this.value = value;
+    }
 }
