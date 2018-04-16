5cdf6e7dcd9453bbcedd950e475fc29fddfde150
Yahya Hussein <yahyahussein91@gmail.com>
2018-04-16 14:57:26 -0700

    created getters and setters

--- Encapsulation/src/com/company/Weapon.java
+++ Encapsulation/src/com/company/Weapon.java
@@ -1,4 +1,31 @@
 package com.company;
 
 public class Weapon {
+    private String name;
+    private int damageInflicted;
+    private int hitPoints;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDamageInflicted() {
+        return damageInflicted;
+    }
+
+    public void setDamageInflicted(int damageInflicted) {
+        this.damageInflicted = damageInflicted;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public void setHitPoints(int hitPoints) {
+        this.hitPoints = hitPoints;
+    }
 }
