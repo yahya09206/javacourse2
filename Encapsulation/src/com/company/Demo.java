9a2a33c6fced1d2a4116e6f2d14889b21da4f801
Yahya Hussein <yahyahussein91@gmail.com>
2018-04-16 15:27:20 -0700

    adding items to enum

--- Encapsulation/src/com/company/Demo.java
+++ Encapsulation/src/com/company/Demo.java
@@ -17,7 +17,5 @@ public class Demo {
         tim.setWeapon(myAxe);
         System.out.println(tim.getWeapon().getName());
 
-
-
     }
 }
---
+++ Encapsulation/src/com/company/Loot.java
@@ -0,0 +1,7 @@
+package com.company;
+
+enum LootType { POTION, RING, ARMOR }
+
+public class Loot {
+
+}
