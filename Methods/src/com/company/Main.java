591af3d9ef5865f1838c80085e45c2c123e341a7
Yahya Hussein <yahyahussein91@gmail.com>
2019-05-15 18:37:07 -0700

    starting on methods

--- Methods/src/com/company/Main.java
+++ Methods/src/com/company/Main.java
@@ -4,5 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        boolean gameOver = true;
+        int score = 10000;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        int score2 = 10000;
+        int levelCompleted2 = 8;
+        int bonus2 = 200;
+
+        if (gameOver){
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
+
+
+        if (gameOver){
+            int finalScore = score2 + (levelCompleted2 * bonus2);
+            System.out.println("Your final score was " + finalScore);
+        }
     }
 }
