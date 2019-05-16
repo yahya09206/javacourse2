122a8187dc3d993d4864c37c1a99abd312773c81
Yahya Hussein <yahyahussein91@gmail.com>
2019-05-15 18:43:43 -0700

    added params to method

--- Methods/src/com/company/Main.java
+++ Methods/src/com/company/Main.java
@@ -9,18 +9,27 @@ public class Main {
         int levelCompleted = 5;
         int bonus = 100;
 
+        calculateScore(true, 800, 5, 100);
+
         int score2 = 10000;
         int levelCompleted2 = 8;
         int bonus2 = 200;
 
         if (gameOver){
-            int finalScore = score + (levelCompleted * bonus);
+            int finalScore = score2 + (levelCompleted2 * bonus2);
             System.out.println("Your final score was " + finalScore);
         }
+    }
 
+    // Method to calculate score
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
+        boolean gameOver = true;
+        int score = 10000;
+        int levelCompleted = 5;
+        int bonus = 100;
 
         if (gameOver){
-            int finalScore = score2 + (levelCompleted2 * bonus2);
+            int finalScore = score + (levelCompleted * bonus);
             System.out.println("Your final score was " + finalScore);
         }
     }
