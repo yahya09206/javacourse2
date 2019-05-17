e9396e4c305742954d2069fb5d121af01da8b72a
Yahya Hussein <yahyahussein91@gmail.com>
2019-05-16 17:51:06 -0700

    set up method for first part of challenge

--- Methods/src/com/company/Main.java
+++ Methods/src/com/company/Main.java
@@ -4,35 +4,40 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
-
-       int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
-        System.out.println("Your final score was " + highScore);
-
-       score = 10000;
-       levelCompleted = 8;
-       bonus = 200;
-
-        highScore = calculateScore(gameOver, score, levelCompleted, bonus);
-        System.out.println("Your final score was " + highScore);
+//        boolean gameOver = true;
+//        int score = 800;
+//        int levelCompleted = 5;
+//        int bonus = 100;
+//
+//       int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+//        System.out.println("Your final score was " + highScore);
+//
+//       score = 10000;
+//       levelCompleted = 8;
+//       bonus = 200;
+//
+//        highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+//        System.out.println("Your final score was " + highScore);
 
 
 
     }
 
     // Method to calculate score
-    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
+//    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
+//
+//        if (gameOver){
+//            int finalScore = score + (levelCompleted * bonus);
+//            finalScore += 2000;
+//            return finalScore;
+//        }else{
+//            return -1;
+//        }
+//    }
 
-        if (gameOver){
-            int finalScore = score + (levelCompleted * bonus);
-            finalScore += 2000;
-            return finalScore;
-        }else{
-            return -1;
-        }
+    // Challenge for Method lecture
+    public static void displayHighScorePosition(String name, int position){
+        System.out.println(name + " managed to get into position " + position + " on the high score table.");
     }
 
 }
