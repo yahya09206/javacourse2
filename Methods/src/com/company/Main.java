2d6a12b16ea7b3f7427e5715cfb92cd040fcdca7
Yahya Hussein <yahyahussein91@gmail.com>
2019-05-16 18:09:02 -0700

    done with challenge

--- Methods/src/com/company/Main.java
+++ Methods/src/com/company/Main.java
@@ -19,6 +19,12 @@ public class Main {
 //        highScore = calculateScore(gameOver, score, levelCompleted, bonus);
 //        System.out.println("Your final score was " + highScore);
 
+        // Method call for challenge
+        displayHighScorePosition("john", calculateHighScorePosition(1500));
+        displayHighScorePosition("john", calculateHighScorePosition(900));
+        displayHighScorePosition("john", calculateHighScorePosition(400));
+        displayHighScorePosition("john", calculateHighScorePosition(50));
+
 
 
     }
@@ -38,6 +44,18 @@ public class Main {
     // Challenge for Method lecture
     public static void displayHighScorePosition(String name, int position){
         System.out.println(name + " managed to get into position " + position + " on the high score table.");
+    }
+
+    public static int calculateHighScorePosition(int score){
+        if (score > 1000){
+            return 1;
+        } else if (score > 500 && score <= 1000){
+            return 2;
+        } else if (score > 100 && score <= 500){
+            return 3;
+        } else {
+            return 4;
+        }
     }
 
 }
