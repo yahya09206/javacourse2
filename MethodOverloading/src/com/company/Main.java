5cfb3a473d4ebd9cf783087b96afcbb6abc8a60b
Yahya Hussein <yahyahussein91@gmail.com>
2019-05-18 16:06:12 -0700

    set up first method to calc ft and inches to cm

--- MethodOverloading/src/com/company/Main.java
+++ MethodOverloading/src/com/company/Main.java
@@ -27,4 +27,11 @@ public class Main {
         return 0;
     }
 
+    public static double calcFeetAndInchesToCentimeters(int feet, double inches){
+        if(feet >= 0 && inches >= 0 && inches <=12){
+            double fttocm = feet/0.032808;
+            double inchtocm = inches/0.39370;
+        }
+        return -1;
+    }
 }
