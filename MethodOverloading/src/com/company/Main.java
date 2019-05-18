e384c7b35599f9ccfc95d3f17c4b1d6b4048e03c
Yahya Hussein <yahyahussein91@gmail.com>
2019-05-18 16:10:42 -0700

    changed conversion formula to match tims

--- MethodOverloading/src/com/company/Main.java
+++ MethodOverloading/src/com/company/Main.java
@@ -27,10 +27,11 @@ public class Main {
         return 0;
     }
 
-    public static double calcFeetAndInchesToCentimeters(int feet, double inches){
+    public static double calcFeetAndInchesToCentimeters(double feet, double inches){
         if(feet >= 0 && inches >= 0 && inches <=12){
-            double fttocm = feet/0.032808;
-            double inchtocm = inches/0.39370;
+            double fttocm = (feet * 30.48);
+            double inchtocm = (inches * 2.54);
+            System.out.println("Feet to cm " + fttocm + ", inches to cm " + inchtocm);
         }
         return -1;
     }
