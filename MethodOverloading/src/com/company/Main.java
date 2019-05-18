b6df582a68ffd29bab04ff1bd3fb09ef36415831
Yahya Hussein <yahyahussein91@gmail.com>
2019-05-18 15:55:27 -0700

    starting on method overload challenge

--- MethodOverloading/src/com/company/Main.java
+++ MethodOverloading/src/com/company/Main.java
@@ -4,5 +4,27 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int newScore = calculateScore("Tim", 500);
+        System.out.println("New score is " + newScore);
+        calculateScore(75);
+        calculateScore();
     }
+
+    // Calculate score method
+    public static int calculateScore(String playerName, int score){
+        System.out.println("Player " + playerName + " scored " + score + " points.");
+        return score * 1000;
+    }
+
+    // Overloaded method
+    public static int calculateScore(int score){
+        System.out.println("Unnamed Player " + " scored " + score + " points.");
+        return score * 1000;
+    }
+
+    public static int calculateScore(){
+        System.out.println("Unnamed Player, no player score.");
+        return 0;
+    }
+
 }
