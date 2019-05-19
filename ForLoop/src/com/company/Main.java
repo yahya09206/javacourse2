3aa247e1881787093c177b2002cedbb14850bc92
Yahya Hussein <yahyahussein91@gmail.com>
2019-05-19 16:18:22 -0700

    method for checking prime numbers

--- ForLoop/src/com/company/Main.java
+++ ForLoop/src/com/company/Main.java
@@ -20,6 +20,19 @@ public class Main {
         for (int i = 8; i >= 2; i--){
             System.out.println("10,000 at " + i + "% interest = " + calculateInterest(10000, i));
         }
+
+    }
+
+    public static boolean isPrime(int n){
+        if (n == 1) {
+            return false;
+        }
+        for (int i = 2; i <= n/2; i++){
+            if (n % 1 == 0){
+                return false;
+            }
+        }
+        return true;
     }
 
     public static double calculateInterest(double amount, double interestRate){
