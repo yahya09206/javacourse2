9651d7bc7cb981b5dca83951619b2576eba9f992
Yahya Hussein <yahyahussein91@gmail.com>
2019-05-19 15:39:35 -0700

    method without for loop

--- ForLoop/src/com/company/Main.java
+++ ForLoop/src/com/company/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("10,000 at 2% interest = " + calculateInterest(10000.00, 2.0));
+    }
+
+    public static double calculateInterest(double amount, double interestRate){
+        return(amount * (interestRate/100));
     }
 }
