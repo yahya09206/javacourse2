fc82bf23833c70676d9c929622064dce8ab07af1
Yahya Hussein <yahyahussein91@gmail.com>
2019-05-19 15:40:52 -0700

    before for loop

--- ForLoop/src/com/company/Main.java
+++ ForLoop/src/com/company/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("10,000 at 2% interest = " + calculateInterest(10000.00, 2.0));
+        System.out.println("10,000 at 3% interest = " + calculateInterest(10000.00, 3.0));
+        System.out.println("10,000 at 4% interest = " + calculateInterest(10000.00, 4.0));
+        System.out.println("10,000 at 5% interest = " + calculateInterest(10000.00, 5.0));
     }
 
     public static double calculateInterest(double amount, double interestRate){
