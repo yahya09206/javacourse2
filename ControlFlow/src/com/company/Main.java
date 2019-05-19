e50a7fc9c89790aaeda15747e68f687a65f62705
Yahya Hussein <yahyahussein91@gmail.com>
2019-05-19 15:02:02 -0700

    completd challenge

--- ControlFlow/src/com/company/Main.java
+++ ControlFlow/src/com/company/Main.java
@@ -15,24 +15,47 @@ public class Main {
 //        }
 
         // Switch statement
-        int switchValue = 3;
-        switch(switchValue){
-            case 1:
-                System.out.println("Value was 1");
-                break;
+//        int switchValue = 3;
+//        switch(switchValue){
+//            case 1:
+//                System.out.println("Value was 1");
+//                break;
+//
+//            case 2:
+//                System.out.println("Value was 2");
+//                break;
+//
+//            //Switch statement shortcut
+//            case 3: case 4: case 5:
+//                System.out.println("was a 3, or 4, or a 5");
+//                System.out.println("Actually it was a " + switchValue);
+//                break;
+//
+//            default:
+//                System.out.println("Was not 1 or 2");
+//                break;
+//        }
 
-            case 2:
-                System.out.println("Value was 2");
+        //Switch statement challenge
+        char switchChar = 'A';
+        switch(switchChar){
+            case 'A':
+                System.out.println("A was found");
                 break;
-
-            //Switch statement shortcut
-            case 3: case 4: case 5:
-                System.out.println("was a 3, or 4, or a 5");
-                System.out.println("Actually it was a " + switchValue);
+            case 'B':
+                System.out.println("B was found");
                 break;
-
+            case 'C':
+                System.out.println("C was found");
+                break;
+            case 'D':
+                System.out.println("D was found");
+                break;
+            case 'E':
+                System.out.println("E was found");
+                break;
             default:
-                System.out.println("Was not 1 or 2");
+                System.out.println("A to E was not found");
                 break;
         }
     }
