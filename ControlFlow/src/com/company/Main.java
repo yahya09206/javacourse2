2c3b79d9b3994613fa2edbb6f7b044335f1432c1
Yahya Hussein <yahyahussein91@gmail.com>
2019-05-19 15:16:10 -0700

    done with switch statement challenge

--- ControlFlow/src/com/company/Main.java
+++ ControlFlow/src/com/company/Main.java
@@ -72,5 +72,37 @@ public class Main {
                 System.out.println("Not sure");
                 break;
         }
+
+        printDayOfTheWeek(5);
+    }
+
+    //Switch statement challenge
+    public static void printDayOfTheWeek(int day){
+        switch (day){
+            case 0:
+                System.out.println("Sunday");
+                break;
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("Invalid day");
+                break;
+        }
     }
 }
