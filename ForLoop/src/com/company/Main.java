70e3f8aa27327c9fe1f4d336b01d0f39aad547f2
Yahya Hussein <yahyahussein91@gmail.com>
2019-05-19 15:58:39 -0700

    for loop reversed challenge completed

--- ForLoop/src/com/company/Main.java
+++ ForLoop/src/com/company/Main.java
@@ -16,6 +16,10 @@ public class Main {
         for (int i = 2; i < 9; i++){
             System.out.println("10,000 at " + i + "% interest = " + calculateInterest(10000, i));
         }
+
+        for (int i = 8; i >= 2; i--){
+            System.out.println("10,000 at " + i + "% interest = " + calculateInterest(10000, i));
+        }
     }
 
     public static double calculateInterest(double amount, double interestRate){
