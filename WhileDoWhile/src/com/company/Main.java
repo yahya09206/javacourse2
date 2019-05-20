00969985dd644f2ea6e4799480c02a05ecb04eb8
Yahya Hussein <yahyahussein91@gmail.com>
2019-05-19 17:28:01 -0700

    first step of challenge complete

--- WhileDoWhile/src/com/company/Main.java
+++ WhileDoWhile/src/com/company/Main.java
@@ -23,5 +23,11 @@ public class Main {
     }
 
     // While loop challenge
-
+    public static boolean isEvenNumber(int num){
+        if(num % 2 == 0){
+            return true;
+        }else {
+            return false;
+        }
+    }
 }
