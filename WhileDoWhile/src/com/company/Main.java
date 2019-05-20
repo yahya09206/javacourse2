c83c0a170a9fb2a8c82811066fdd3ac685e23a31
Yahya Hussein <yahyahussein91@gmail.com>
2019-05-19 17:25:34 -0700

    starting on while loop challenge

--- WhileDoWhile/src/com/company/Main.java
+++ WhileDoWhile/src/com/company/Main.java
@@ -4,5 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int count = 0;
+        while(count !=6 ){
+            System.out.println("Count value is " + count);
+            count++;
+        }
+
+        count = 6;
+        do {
+            System.out.println("Count value was " + count);
+            count++;
+
+            if (count > 100){
+                break;
+            }
+
+        }while (count != 6);
     }
+
+    // While loop challenge
+
 }
