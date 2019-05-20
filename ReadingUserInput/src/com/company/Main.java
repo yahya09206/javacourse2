8c726427bc20a54fb7dd0b5399fa9955475e69a2
Yahya Hussein <yahyahussein91@gmail.com>
2019-05-19 19:41:27 -0700

    bug preventing from typing name

--- ReadingUserInput/src/com/company/Main.java
+++ ReadingUserInput/src/com/company/Main.java
@@ -5,13 +5,21 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
     //Set up scanner
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your year of birth: ");
+        int yearOfBirth = scanner.nextInt();
+
+
         System.out.println("Enter you name: ");
-        //Read line of input
+        //Read line of string input
         String name = scanner.nextLine();
-        System.out.println("Your name is " + " " + name);
+        //Calculate age
+        int age = 2019 - yearOfBirth;
+
+        //Print out input
+        System.out.println("Your name is " + " " + name + ", and you are " + age + " years old.");
 
         //close scanner
         scanner.close();
