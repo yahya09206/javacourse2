8735891852b2db5466f66850bc6674e290d83122
Yahya Hussein <yahyahussein91@gmail.com>
2019-05-19 19:36:58 -0700

    scanner accepts and outputs

--- ReadingUserInput/src/com/company/Main.java
+++ ReadingUserInput/src/com/company/Main.java
@@ -8,5 +8,12 @@ public class Main {
 	// write your code here
     //Set up scanner
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter you name: ");
+        //Read line of input
+        String name = scanner.nextLine();
+        System.out.println("Your name is " + " " + name);
+
+        //close scanner
+        scanner.close();
     }
 }
