21d1fefd705109d5bc51b80d605d9130f05ec391
Yahya Hussein <yahyahussein91@gmail.com>
2019-05-19 19:33:26 -0700

    set up new instance of scanner

---
+++ ReadingUserInput/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    //Set up scanner
+        Scanner scanner = new Scanner(System.in);
+    }
+}
