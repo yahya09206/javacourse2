3042751c519f53d1b52b98e78450efdeb69ed961
Yahya Hussein <yahyahussein91@gmail.com>
2019-05-24 18:37:22 -0700

    set up method to deposit funds

--- Classes/src/com/company/BankAccount.java
+++ Classes/src/com/company/BankAccount.java
@@ -7,4 +7,50 @@ public class BankAccount {
     private String email;
     private String phoneNumber;
 
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void depositFunds(double depositAmount){
+        this.balance += depositAmount;
+        System.out.println("Your new balance is " + this.balance);
+    }
+
+
 }
