310cdc2a2f9690cf7af9deb6e8fa3e4991f15c52
Yahya Hussein <yahyahussein91@gmail.com>
2019-05-24 18:30:38 -0700

    created fields for bank account class

---
+++ Classes/src/com/company/BankAccount.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class BankAccount {
+    private int accountNumber;
+    private double balance;
+    private String name;
+    private String email;
+    private String phoneNumber;
+
+}
