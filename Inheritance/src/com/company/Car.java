aa161f8e15951980ef2cad301bc49fa6fbdd72cb
Yahya Hussein <yahyahussein91@gmail.com>
2019-05-25 19:34:59 -0700

    setting up car class

--- Inheritance/src/com/company/Car.java
+++ Inheritance/src/com/company/Car.java
@@ -1,5 +1,11 @@
 package com.company;
 
 public class Car extends Vehicle{
+    private String gasType;
+    private int milesPerHour;
+    private String carType
 
+    public Car(String name, int doors, int wheels, int steeringWheel) {
+        super(name, doors, wheels, steeringWheel);
+    }
 }
