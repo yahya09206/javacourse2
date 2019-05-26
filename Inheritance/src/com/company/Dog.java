4c39c4138d4adb513edb49c3fde167b1b4681236
Yahya Hussein <yahyahussein91@gmail.com>
2019-05-25 18:16:49 -0700

    set up non inherited fields for dog class

---
+++ Inheritance/src/com/company/Dog.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Dog extends Animal {
+    private int eyes;
+    private int legs;
+    private int tail;
+    private int teeth;
+    private String coat;
+
+    // Constructor for other class
+    public Dog(int brain, int body, int size, int weight, String name) {
+        super(brain, body, size, weight, name);
+    }
+}
