405dd2ff1a05a50f2e3b5a8a92c2cc1c71c26149
Yahya Hussein <yahyahussein91@gmail.com>
2019-05-31 19:11:55 -0700

    completed sortArray method

--- Arrays/src/com/company/Main.java
+++ Arrays/src/com/company/Main.java
@@ -33,6 +33,20 @@ public class Main {
         for (int i = 0; i < array.length; i++){
             sortedArray[i] = array[i];
         }
+        boolean flag = true;
+        int temp;
+        while (flag){
+            flag = false;
+            for (int i = 0; i < sortedArray.length - 1; i++){
+                if (sortedArray[i] < sortedArray[i+1]){
+                    temp = sortedArray[i];
+                    sortedArray[i] = sortedArray[i+1];
+                    sortedArray[i+1] = temp;
+                    flag = true;
+                }
+            }
+        }
+        return sortedArray;
     }
 
 
