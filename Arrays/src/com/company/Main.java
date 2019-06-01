79edbcf2c8debd89764e12daa163bebd7d7cee66
Yahya Hussein <yahyahussein91@gmail.com>
2019-05-31 19:16:29 -0700

    sort array works

--- Arrays/src/com/company/Main.java
+++ Arrays/src/com/company/Main.java
@@ -9,6 +9,8 @@ public class Main {
 
     public static void main(String[] args) {
         int[] myIntegers = getIntegers(5);
+        int[] sorted = sortIntegers(myIntegers);
+        printArray(sorted);
 
 
     }
