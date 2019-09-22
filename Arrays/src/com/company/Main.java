79cb6eb4fdd132a82b56355bc5926885a3d2e656
Yahya Hussein <yahyahussein91@gmail.com>
2019-09-21 22:35:48 -0700

    defining array

--- Arrays/src/com/company/Main.java
+++ Arrays/src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        // define array
+        int[] myIntArray = new int[10];
+        // Save value of 50 into element 6 since arrays have a 0-based index
+        myIntArray[5] = 50;
+        double[] myDoubleArray = new double[10];
+
     }
 }
