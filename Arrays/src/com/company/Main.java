8e32cf4098d7617644d6a172195b15e6f0e13b5b
Yahya Hussein <yahyahussein91@gmail.com>
2019-09-21 22:54:52 -0700

    method for finding average of array

--- Arrays/src/com/company/Main.java
+++ Arrays/src/com/company/Main.java
@@ -8,6 +8,9 @@ public class Main {
     public static void main(String[] args) {
 
         int[] myIntegers = getIntegers(5);
+        for (int i = 0; i < myIntegers.length; i++){
+            System.out.println("Element " + i + ", typed value was " + myIntegers[i]);
+        }
 
     }
 
@@ -18,7 +21,18 @@ public class Main {
         for (int i = 0; i < values.length; i++){
             values[i] = scanner.nextInt();
         }
+
         return values;
+    }
+
+    // Method to return average of array
+    public static double getAverage(int[] array) {
+        int sum = 0;
+        for(int i = 0; i < array.length; i++){
+            sum += array[i];
+        }
+
+        return (double) sum / (double) array.length;
     }
 }
 
