4036b244b3a2f1a48c04d4d076c3cb55e6f56d5b
Yahya Hussein <yahyahussein91@gmail.com>
2019-09-21 22:51:43 -0700

    more methods with arrays

--- Arrays/src/com/company/Main.java
+++ Arrays/src/com/company/Main.java
@@ -1,29 +1,44 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
+    private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        // define array
-        int[] myIntArray =  new int[25];//{1,2,3,4,5,6,7,8,9,10};
-        // Save value of 50 into element 6 since arrays have a 0-based index
-//        myIntArray[5] = 50;
-//        double[] myDoubleArray = new double[10];
-//        System.out.println(myIntArray[0]);
-//        System.out.println(myIntArray[6]);
-//        System.out.println(myIntArray[8]);
 
-        // use for loop to run through array
-        for (int i = 0; i < myIntArray.length; i++) {
-            myIntArray[i] = i * 10;
-        }
-        printArray(myIntArray);
+        int[] myIntegers = getIntegers(5);
 
     }
 
-    public static void printArray(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            System.out.println("Element " + i + ", value is " + array[i]);
-        }
-    }
+    public static int[] getIntegers(int number) {
+        System.out.println("Enter " + number + " integer values.\r");
+        int[] values = new int[number];
 
+        for (int i = 0; i < values.length; i++){
+            values[i] = scanner.nextInt();
+        }
+        return values;
+    }
 }
+
+// public static void printArray(int[] array) {
+//        for (int i = 0; i < array.length; i++) {
+//            System.out.println("Element " + i + ", value is " + array[i]);
+//        }
+//    }
+
+// // define array
+//        int[] myIntArray =  new int[25];//{1,2,3,4,5,6,7,8,9,10};
+//        // Save value of 50 into element 6 since arrays have a 0-based index
+////        myIntArray[5] = 50;
+////        double[] myDoubleArray = new double[10];
+////        System.out.println(myIntArray[0]);
+////        System.out.println(myIntArray[6]);
+////        System.out.println(myIntArray[8]);
+//
+//        // use for loop to run through array
+//        for (int i = 0; i < myIntArray.length; i++) {
+//            myIntArray[i] = i * 10;
+//        }
+//        printArray(myIntArray);
