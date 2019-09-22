96c712fee826e318a2062f72a32e8f819cec6b79
Yahya Hussein <yahyahussein91@gmail.com>
2019-09-21 22:47:08 -0700

    created method to print array

--- Arrays/src/com/company/Main.java
+++ Arrays/src/com/company/Main.java
@@ -4,10 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         // define array
-        int[] myIntArray =  new int[10];//{1,2,3,4,5,6,7,8,9,10};
+        int[] myIntArray =  new int[25];//{1,2,3,4,5,6,7,8,9,10};
         // Save value of 50 into element 6 since arrays have a 0-based index
 //        myIntArray[5] = 50;
-        double[] myDoubleArray = new double[10];
+//        double[] myDoubleArray = new double[10];
 //        System.out.println(myIntArray[0]);
 //        System.out.println(myIntArray[6]);
 //        System.out.println(myIntArray[8]);
@@ -16,8 +16,14 @@ public class Main {
         for (int i = 0; i < myIntArray.length; i++) {
             myIntArray[i] = i * 10;
         }
-        for (int i = 0; i < myIntArray.length; i++) {
-            System.out.println("Element " + i + ", value is " + myIntArray[i]);
+        printArray(myIntArray);
+
+    }
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Element " + i + ", value is " + array[i]);
         }
     }
+
 }
