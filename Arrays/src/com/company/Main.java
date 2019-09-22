fd40b39f210b1f75b1466ee627d3dbc666008b21
Yahya Hussein <yahyahussein91@gmail.com>
2019-09-21 22:41:45 -0700

    used for loop to access elements

--- Arrays/src/com/company/Main.java
+++ Arrays/src/com/company/Main.java
@@ -4,10 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
         // define array
-        int[] myIntArray = new int[10];
+        int[] myIntArray =  new int[10];//{1,2,3,4,5,6,7,8,9,10};
         // Save value of 50 into element 6 since arrays have a 0-based index
-        myIntArray[5] = 50;
+//        myIntArray[5] = 50;
         double[] myDoubleArray = new double[10];
+//        System.out.println(myIntArray[0]);
+//        System.out.println(myIntArray[6]);
+//        System.out.println(myIntArray[8]);
 
+        // use for loop to run through array
+        for (int i = 0; i < 10; i++) {
+            myIntArray[i] = i * 10;
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Element " + i + ", value is " + myIntArray[i]);
+        }
     }
 }
