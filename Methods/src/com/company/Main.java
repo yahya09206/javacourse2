15ee6a3cd45c1f2888b3f8ce9f9f2f6fa6672a65
Yahya Hussein <yahyahussein91@gmail.com>
2019-12-09 17:45:42 -0800

    done with basic methods

--- Methods/src/com/company/Main.java
+++ Methods/src/com/company/Main.java
@@ -3,29 +3,27 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    boolean gameOver = true;
-	    int score = 100;
-	    int levelCompleted = 5;
-	    int bonus = 100;
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
 
-	    calculateScore(true, 100, 6, 25000);
+        int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your final score was " + highScore);
 
-	    score = 10000;
-	    levelCompleted = 8;
-	    bonus = 200;
 
-	    if (gameOver){
-	        int finalScore = score + (levelCompleted * bonus);
-            System.out.println("Your final score was " + finalScore);
-        }
+        calculateScore(true, 100, 6, 25000);
+        calculateScore(true, 10000,8,200);
+
     }
 
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
 
         if (gameOver){
             int finalScore = score + (levelCompleted * bonus);
-            finalScore += 1000;
-            System.out.println("Your final score was " + finalScore);
+            finalScore += 2000;
+            return finalScore;
         }
+        return -1;
     }
 }
