6e9fe6fe84f310d01b1e0e0d8f96ed431f676aa0
Yahya Hussein <yahyahussein91@gmail.com>
2019-12-09 16:53:55 -0800

    finished with intro

--- Intro/src/com/company/Main.java
+++ Intro/src/com/company/Main.java
@@ -3,6 +3,25 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        char myChar = 'D';
+
+
     }
 }
+// write your code here
+//        System.out.println("Hello world");
+//
+//                // Variable
+//                int myFirstNumber = (10 + 5) + (2 * 10);
+//                int mySecondNumber = 12;
+//                int myThirdNumber = myFirstNumber * 2;
+//                int sum = mySecondNumber + myThirdNumber + myFirstNumber;
+//                int myLastOne = 1000 - sum;
+//                System.out.println(sum);
+//                System.out.println(myLastOne);
+//
+//                byte myMinByteValue = Byte.MIN_VALUE;
+//                byte myMaxByteValue = Byte.MAX_VALUE;
+//                System.out.println("Byte Min Value = " + myMinByteValue);
+//                System.out.println("Byte Max Value = " + myMaxByteValue);
