b67f93ae85c129ce7a35fa453422c0ad56491113
Yahya Hussein <yahyahussein91@gmail.com>
2020-06-07 14:50:16 -0700

    done with first run of method challenge

--- Methods/src/com/company/Main.java
+++ Methods/src/com/company/Main.java
@@ -24,4 +24,21 @@ public class Main {
         return -1;
     }
 
+    //Method challenge
+    public static void displayHighScorePosition(String name, int position){
+        System.out.println(name + " managed to get into position " + position + " on the high score table");
+    }
+
+    public static int calculateHighScorePosition(int playerScore){
+        if(playerScore > 1000){
+            return 1;
+        }else if(playerScore > 500 && playerScore < 1000){
+            return 2;
+        }else if(playerScore > 100 && playerScore < 500){
+            return 3;
+        }else{
+            return 4;
+        }
+    }
+
 }
