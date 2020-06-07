b6cb29d245115801a63ae887cd0e4b4dec495dd4
Yahya Hussein <yahyahussein91@gmail.com>
2020-06-07 15:25:57 -0700

    done with method overloading

--- MethodOverLoading/src/com/company/Main.java
+++ MethodOverLoading/src/com/company/Main.java
@@ -4,5 +4,25 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int newScore = calculateScore("yo", 500);
+        System.out.println("New score is " + newScore);
+        calculateScore(75);
+        calculateScore();
     }
+
+    public static int calculateScore(String name, int score){
+        System.out.println("Player " + name + " scored " + score + " points");
+        return score * 1000;
+    }
+
+    public static int calculateScore(int score){
+        System.out.println("unnamed player scored " + score + " points");
+        return score * 1000;
+    }
+
+    public static int calculateScore(){
+        System.out.println("no player name, no player points");
+        return 0;
+    }
+
 }
