44d540d7f000a18d9b220d07e098a287920244a1
Yahya Hussein <yahyahussein91@gmail.com>
2020-08-14 20:10:57 -0700

    done with for loop section

--- ForLoop/src/com/company/Main.java
+++ ForLoop/src/com/company/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        for (int i = 2; i <= 8; i ++){
+        for (int i = 2; i <= 8; i++){
+            System.out.println("10,000 at " + i + "% interest = " + calculateInterest(10000.0, i));
+        }
+
+        for (int i = 8; i >= 2; i--){
             System.out.println("10,000 at " + i + "% interest = " + calculateInterest(10000.0, i));
         }
 
