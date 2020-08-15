f49ed9de9089c1cf71ab5958cf6353ef6c22cc0a
Yahya Hussein <yahyahussein91@gmail.com>
2020-08-14 19:55:24 -0700

    completed for loop mini challenge

--- ForLoop/src/com/company/Main.java
+++ ForLoop/src/com/company/Main.java
@@ -4,5 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        for (int i = 2; i <= 8; i ++){
+            System.out.println("10,000 at " + i + "% interest = " + calculateInterest(10000.0, i));
+        }
+
+    }
+
+    public static double calculateInterest(double amount, double interestRate){
+        return(amount * (interestRate/100));
     }
 }
