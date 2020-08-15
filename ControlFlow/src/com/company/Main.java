ee618a894fb5d3419371035aecc0cdc9e71e94ae
Yahya Hussein <yahyahussein91@gmail.com>
2020-08-14 19:10:51 -0700

    internet cut off

--- ControlFlow/src/com/company/Main.java
+++ ControlFlow/src/com/company/Main.java
@@ -4,5 +4,26 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int value = 1;
+        if(value === 1){
+            System.out.println("Value was 1")
+        }else  if(value === 2){
+            System.out.println("Value was 3")
+        }else {
+            System.out.println("Was not 1 or 2");
+        }
+
+        //Switch Statement
+        int switchValue = 1;
+
+        switch(switchValue) {
+            case 1:
+                System.out.println("Value was 1");
+                break;
+
+            case 2:
+                System.out.println("Value was 2");
+                break;
+        }
     }
 }
