2e2a7546c76ccabb555b9769686c9a2e3f0d5ea1
Yahya Hussein <yahyahussein91@gmail.com>
2020-08-17 19:36:40 -0700

    new section on while loops

---
+++ WhileDoWhile/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int count = 1;
+        while(count != 5){
+            System.out.println("Count value is " + count);
+            count++;
+        }
+
+    }
+}
