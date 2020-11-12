91fd2d6895f36084ccdf2c643ed70c542e517f2e
Yahya Hussein <yahyahussein91@gmail.com>
2020-11-12 15:43:51 -0800

    done with switch statement challenge

--- ControlFlow/src/com/company/Main.java
+++ ControlFlow/src/com/company/Main.java
@@ -3,6 +3,53 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    int value = 1;
+	    if (value == 1){
+            System.out.println("Value was 1");
+        }else if (value == 2){
+            System.out.println("Value was 2");
+        }else {
+            System.out.println("Value was 3");
+        }
+
+	    //Switch statement to clean up preceeding code
+        int switchValue = 1;
+
+	    switch(switchValue){
+            case 1:
+                System.out.println("Value was 1");
+                break;
+            case 2:
+                System.out.println("Value was 2");
+                break;
+            default:
+                System.out.println("Value was 3");
+                break;
+        }
+
+        //Switch statement challenge
+        char charValue = 'A';
+
+	    switch (charValue){
+            case 'A':
+                System.out.println("Value was A");
+                break;
+            case 'B':
+                System.out.println("Value was B");
+                break;
+            case 'C':
+                System.out.println("Value was C");
+                break;
+            case 'D':
+                System.out.println("Value was D");
+                break;
+            case 'E':
+                System.out.println("Value was E");
+                break;
+            default:
+                System.out.println("None of the values were found");
+                break;
+        }
+
     }
 }
