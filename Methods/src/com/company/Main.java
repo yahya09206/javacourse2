32f32c5f10d0284e14d0077d2c3c528bb7c8ea03
Yahya Hussein <yahyahussein91@gmail.com>
2020-11-11 19:07:50 -0800

    done with basic methods

--- Methods/src/com/company/Main.java
+++ Methods/src/com/company/Main.java
@@ -4,16 +4,59 @@ public class Main {
 
     public static void main(String[] args) {
 
-        calculateScore(true, 800, 5, 100);
-        calculateScore(true, 10000, 8, 200);
+        int highScore = calculateScore(true, 800, 5, 100);
+        System.out.println("Your final score was " + highScore);
+
+        highScore = calculateScore(true, 10000, 8, 200);
+        System.out.println("Your final score was " + highScore);
+
+        int highScorePosition = calculateHighScorePosition(1500);
+        displayHighScorePosition("Mikey", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(900);
+        displayHighScorePosition("James", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(400);
+        displayHighScorePosition("Bob", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(50);
+        displayHighScorePosition("John", highScorePosition);
+
+        displayHighScorePosition("Mikey", calculateHighScorePosition(1500));
+        displayHighScorePosition("Mikey", calculateHighScorePosition(900));
+        displayHighScorePosition("Mikey", calculateHighScorePosition(400));
+        displayHighScorePosition("Mikey", calculateHighScorePosition(50));
+
+
     }
 
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
         if (gameOver){
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 1000;
-            System.out.println("Your final score was " + finalScore);
+            return finalScore;
+        }else {
+            return -1;
         }
     }
+
+    //Method challenge
+    public static  void displayHighScorePosition(String name, int position){
+        System.out.println(name + " managed to get into position " + position);
+    }
+
+    public static int calculateHighScorePosition(int playerScore){
+        if (playerScore >= 1000){
+            return 1;
+        }else if(playerScore >= 500 && playerScore < 1000){
+            return 2;
+        }else if(playerScore >= 100 && playerScore < 500){
+            return 3;
+        }else {
+            return 4;
+        }
+    }
+
+
 }
 //Statement is the entire line of code
