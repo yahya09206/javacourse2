5939586c9072a72a0bbb35b6d5ec98dfa8de7299
Yahya Hussein <yahyahussein91@gmail.com>
2020-11-11 20:25:32 -0800

    refactoring

--- MethodOverLoading/src/com/company/Main.java
+++ MethodOverLoading/src/com/company/Main.java
@@ -33,7 +33,7 @@ public class Main {
             //Convert feet and inches to centimeters
             double centimeters = feet * 30.48;
             centimeters += inches * 2.54;
-            System.out.println(feet + " ft  " + inches + "inches = " + centimeters + "cm");
+            System.out.println(feet + " ft " + inches + "inches = " + centimeters + "cm");
             return centimeters;
         }else {
             return -1;
@@ -44,7 +44,7 @@ public class Main {
         if(inches >= 0){
             double feet = (int) inches / 12;
             double remainingInches = (int) inches % 12;
-            System.out.println(inches + "inches is equal to " + feet + "ft and " + remainingInches +"inches");
+            System.out.println(inches + " inches is equal to " + feet + "ft and " + remainingInches +" inches");
             return calcFeetAndInchesToCentimeters(feet, remainingInches);
         }else {
             return -1;
