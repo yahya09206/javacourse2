f86095f52009f38e65b7c680ddad6d3f99ab9728
Yahya Hussein <yahyahussein91@gmail.com>
2020-11-11 16:17:46 -0800

    quick refresher on if statements

--- Methods/src/com/company/Main.java
+++ Methods/src/com/company/Main.java
@@ -4,5 +4,32 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+//        if (score < 5000 && score > 1000) {
+//            System.out.println("Your score is 5000 but greater than 1000");
+//        } else if (score < 1000){
+//                System.out.println("Your score was less than 1000");
+//        } else {
+//            System.out.println("Got here");
+//        }
+
+        if (gameOver){
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
+
+        //If statement challenge
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+        if (gameOver){
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
     }
 }
+//Statement is the entire line of code
