775876b58822881319b98b76ba7a601a216ea43d
Yahya Hussein <yahyahussein91@gmail.com>
2020-11-11 16:31:53 -0800

    refactored code using a method

--- Methods/src/com/company/Main.java
+++ Methods/src/com/company/Main.java
@@ -3,31 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
 
-//        if (score < 5000 && score > 1000) {
-//            System.out.println("Your score is 5000 but greater than 1000");
-//        } else if (score < 1000){
-//                System.out.println("Your score was less than 1000");
-//        } else {
-//            System.out.println("Got here");
-//        }
+        calculateScore(true, 800, 5, 100);
+        calculateScore(true, 10000, 8, 200);
+    }
 
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
         if (gameOver){
             int finalScore = score + (levelCompleted * bonus);
-            System.out.println("Your final score was " + finalScore);
-        }
-
-        //If statement challenge
-        score = 10000;
-        levelCompleted = 8;
-        bonus = 200;
-        if (gameOver){
-            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 1000;
             System.out.println("Your final score was " + finalScore);
         }
     }
