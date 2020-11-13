d14f8c8467c7eaf0fa513d3adbed6000d6650886
Yahya Hussein <yahyahussein91@gmail.com>
2020-11-12 19:41:27 -0800

    memory still there so far

--- ControlFlow/ForLoops/src/com/company/Main.java
+++ ControlFlow/ForLoops/src/com/company/Main.java
@@ -10,12 +10,57 @@ public class Main {
             System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f", calculateInterest(10000.0, i)));
         }
 
+        System.out.println("***************");
+
         for (int i = 8; i >= 2; i--){
             System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f", calculateInterest(10000.0, i)));
         }
+
+        int count = 0;
+        for (int i = 1; i <= 50; i++){
+            if(isPrime(i)){
+                System.out.println(i);
+                count++;
+                if (count == 3){
+                    break;
+                }
+            }
+        }
+        System.out.println(count + " prime numbers were found");
+
+
+        //Sum 3 and 5 challenge
+        int counts = 0;
+        int sum = 0;
+        for (int i = 1; i <= 1000; i++){
+            //check which numbers are divisible by 3 and 5
+            if (i % 3 == 0 && i % 5 == 0){
+                System.out.println(i);
+                counts++;
+                sum += i;
+                if(counts == 5){
+                    break;
+                }
+            }
+        }
+        System.out.println(sum);
+
     }
 
     public static double calculateInterest(double amount, double interestRate){
         return (amount * (interestRate / 100));
+    }
+
+    //For loop real challenge
+    public static boolean isPrime(int n){
+        if (n == 1){
+            return false;
+        }
+        for (int i = 2; i <= n / 2; i++){
+            if (n % i == 0){
+                return false;
+            }
+        }
+        return true;
     }
 }
