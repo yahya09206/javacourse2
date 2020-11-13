94d1652e3d4c8c64898f7a58b463063e0d0188b8
Yahya Hussein <yahyahussein91@gmail.com>
2020-11-12 19:06:37 -0800

    done with first foor loop challenge

--- ControlFlow/ForLoops/src/com/company/Main.java
+++ ControlFlow/ForLoops/src/com/company/Main.java
@@ -4,6 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 2.0));
 
+        for (int i = 2; i <= 8; i++){
+            System.out.println("10,000 at " + i + "% interest = " + calculateInterest(10000.0, i));
+        }
+    }
+
+    public static double calculateInterest(double amount, double interestRate){
+        return (amount * (interestRate / 100));
     }
 }
