5fc1f39fb2699c78017510a2be26a27b72510ee1
Yahya Hussein <yahyahussein91@gmail.com>
2020-12-01 15:07:15 -0800

    done with challenge

--- ClassesInheritanceConstructors/Inheritance/src/com/company/Main.java
+++ ClassesInheritanceConstructors/Inheritance/src/com/company/Main.java
@@ -4,5 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Bank bobsBank = new Bank();
+        bobsBank.withdrawFunds(100.00);
+
+        bobsBank.depositFunds(50.0);
+        bobsBank.withdrawFunds(100.00);
+
+        bobsBank.depositFunds(51.0);
+        bobsBank.withdrawFunds(100.00);
     }
 }
