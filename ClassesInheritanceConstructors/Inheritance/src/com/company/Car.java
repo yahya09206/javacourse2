3c5474181e5dc98b9dfb2d025298d1d2dcb05241
Yahya Hussein <yahyahussein91@gmail.com>
2020-12-03 22:44:08 -0800

    setting up car class that extends vehicle

--- ClassesInheritanceConstructors/Inheritance/src/com/company/Car.java
+++ ClassesInheritanceConstructors/Inheritance/src/com/company/Car.java
@@ -2,12 +2,10 @@ package com.company;
 
 public class Car extends Vehicle {
 
-    private int windows;
+    private int wheels;
     private int doors;
+    private int gears;
+    private boolean isManual;
 
-    public Car(int wheels, int doors, int trunk, String name, int windows, String fuelType, int windows1, int doors1) {
-        super(wheels, doors, trunk, name, windows, fuelType);
-        this.windows = windows1;
-        this.doors = doors1;
-    }
+    private int currentGear;
 }
