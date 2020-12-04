0b1f21f04f62e8ba0c330920f6f868588b1e1a47
Yahya Hussein <yahyahussein91@gmail.com>
2020-12-03 22:43:14 -0800

    set up vehicle class

--- ClassesInheritanceConstructors/Inheritance/src/com/company/Car.java
+++ ClassesInheritanceConstructors/Inheritance/src/com/company/Car.java
@@ -1,4 +1,13 @@
 package com.company;
 
-public class Car {
+public class Car extends Vehicle {
+
+    private int windows;
+    private int doors;
+
+    public Car(int wheels, int doors, int trunk, String name, int windows, String fuelType, int windows1, int doors1) {
+        super(wheels, doors, trunk, name, windows, fuelType);
+        this.windows = windows1;
+        this.doors = doors1;
+    }
 }
