bd8a4292a18118ae56a47901188862891fdb8233
Yahya Hussein <yahyahussein91@gmail.com>
2020-12-03 21:55:38 -0800

    created animal class with constructor and getters

---
+++ ClassesInheritanceConstructors/Inheritance/src/com/company/Animal.java
@@ -0,0 +1,38 @@
+package com.company;
+
+public class Animal {
+
+    private int name;
+    private int brain;
+    private int body;
+    private int size;
+    private int weight;
+
+    public Animal(int name, int brain, int body, int size, int weight) {
+        this.name = name;
+        this.brain = brain;
+        this.body = body;
+        this.size = size;
+        this.weight = weight;
+    }
+
+    public int getName() {
+        return name;
+    }
+
+    public int getBrain() {
+        return brain;
+    }
+
+    public int getBody() {
+        return body;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+}
