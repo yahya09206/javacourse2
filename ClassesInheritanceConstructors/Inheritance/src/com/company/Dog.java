08d3eae100f5fa78b41e64db98d83669e9e7ef93
Yahya Hussein <yahyahussein91@gmail.com>
2020-12-03 21:57:19 -0800

    created constructor for new dog class

---
+++ ClassesInheritanceConstructors/Inheritance/src/com/company/Dog.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Dog extends Animal{
+    public Dog(int name, int brain, int body, int size, int weight) {
+        super(name, brain, body, size, weight);
+    }
+}
