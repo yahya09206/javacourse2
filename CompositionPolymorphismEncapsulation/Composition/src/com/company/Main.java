ade2c0083ab9506d8e3df77378f78d40cae9d856
Yahya Hussein <yahyahussein91@gmail.com>
2020-12-05 14:53:38 -0800

    done with composition part 1

--- CompositionPolymorphismEncapsulation/Composition/src/com/company/Main.java
+++ CompositionPolymorphismEncapsulation/Composition/src/com/company/Main.java
@@ -4,6 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        Dimensions dimensions = new Dimensions(20, 20, 5);
+        Case newCase = new Case("220B", "Dell", "240", dimensions);
+
+        Monitor theMonitor = new Monitor("27 inch Beast", "Acer", 27, new Resolution(2540, 1440));
+
+        MotherBoard theMotherBoard = new MotherBoard("BJ-200", "Asus", 4,6,"v2.44");
+
+        PC thePC = new PC(newCase, theMonitor, theMotherBoard);
+//        thePC.getMonitor().drawPixelAt(1500, 1200, "red");
+//        thePC.getMotherBoard().loadProgram("Windows 1.0");
+//        thePC.getTheCase().pressPowerButton();
+        thePC.powerUp();
     }
 }
 
@@ -12,3 +25,4 @@ public class Main {
         //meaning a car is a vehicle
 
 //Composition is a Has-A relationship
+    //general rule is to look at composition before using inheritance
---
+++ CompositionPolymorphismEncapsulation/Composition/src/com/company/PC.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class PC {
+
+    private Case theCase;
+    private Monitor monitor;
+    private MotherBoard motherBoard;
+
+    public PC(Case theCase, Monitor monitor, MotherBoard motherBoard) {
+        this.theCase = theCase;
+        this.monitor = monitor;
+        this.motherBoard = motherBoard;
+    }
+
+    public void powerUp(){
+        theCase.pressPowerButton();
+        drawLogo();
+    }
+
+    private void drawLogo(){
+        // Fancy graphics;
+        monitor.drawPixelAt(1200, 50, "yellow");
+    }
+
+//    private Case getTheCase() {
+//        return theCase;
+//    }
+//
+//    private Monitor getMonitor() {
+//        return monitor;
+//    }
+//
+//    private MotherBoard getMotherBoard() {
+//        return motherBoard;
+//    }
+}
