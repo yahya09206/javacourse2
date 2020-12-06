abd03aaade686af3800ba106e42417589374c6a0
Yahya Hussein <yahyahussein91@gmail.com>
2020-12-05 19:49:45 -0800

    created mobile phone class to implement interface

--- InnerAbstractInterfaces/Interfaces/src/com/company/Main.java
+++ InnerAbstractInterfaces/Interfaces/src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        ITelephone timsPhone;
+        timsPhone = new DeskPhone(2063030838);
+        timsPhone.powerOn();
+        timsPhone.callPhone(2063030838);
+        timsPhone.answer();
     }
 }
--- InnerAbstractInterfaces/Interfaces/src/com/company/MobilePhone.java
+++ InnerAbstractInterfaces/Interfaces/src/com/company/MobilePhone.java
@@ -1,37 +1,43 @@
 package com.company;
 
-public class DeskPhone implements ITelephone {
+public class MobilePhone implements ITelephone {
 
     private int myNumber;
     private boolean isRinging;
+    private boolean isOn = false;
 
-    public DeskPhone(int myNumber) {
+    public MobilePhone(int myNumber) {
         this.myNumber = myNumber;
     }
 
     @Override
     public void powerOn() {
-        System.out.println("No action taken, this phone does not have a power button");
+        isOn = true;
+        System.out.println("Mobile phone powered up");
     }
 
     @Override
     public void dial(int phoneNumber) {
-        System.out.println("Now ringing " + phoneNumber + " on deskphone");
+        if (isOn) {
+            System.out.println("Now ringing " + phoneNumber + " on deskphone");
+        }else {
+            System.out.println("Phone is switched off");
+        }
     }
 
     @Override
     public void answer() {
         if(isRinging){
-            System.out.println("Answering the deskphone");
-            isRinging = true;
+            System.out.println("Answering the mobile phone");
+            isRinging = false;
         }
     }
 
     @Override
     public boolean callPhone(int phoneNumber) {
-        if (phoneNumber == myNumber){
+        if (phoneNumber == myNumber && isOn){
             isRinging = true;
-            System.out.println("Ring ring");
+            System.out.println("Melody playing");
         }else {
             isRinging = false;
         }
@@ -43,3 +49,4 @@ public class DeskPhone implements ITelephone {
         return isRinging;
     }
 }
+
