7cf722b71f5795b5aca6544e103358b75f51b17e
Yahya Hussein <yahyahussein91@gmail.com>
2020-12-08 19:31:54 -0800

    added grocery class with arraylist methods

---
+++ ArrayLists/src/com/company/GroceryList.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class GroceryList {
+    // Declare ArrayList and initialize it
+    private ArrayList<String> groceryList = new ArrayList<>();
+
+    // Function to add a grocery item
+    public void addGroceryItem(String item){
+        // Add to list
+        groceryList.add(item);
+    }
+
+    // Print list
+    public void printGroceryList(){
+        System.out.println("You have " + groceryList.size() + " items in your grocery list");
+        for (int i = 0; i < groceryList.size(); i++){
+            System.out.println((i + 1) + ". " + groceryList.get(i));
+        }
+    }
+
+    // Replace an item in the list
+    public void modifyGroceryItem(int position, String newItem){
+        groceryList.set(position, newItem);
+        System.out.println("Grocery item " + (position + 1) + " has been modified");
+    }
+
+    // Remove an item
+    public void modifyGroceryItem(int position){
+        String theItem = groceryList.get(position);
+        groceryList.remove(position);
+    }
+}
