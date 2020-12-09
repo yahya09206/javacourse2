476a43f97388a3d624f5c181a169586af923fb36
Yahya Hussein <yahyahussein91@gmail.com>
2020-12-08 20:47:18 -0800

    added method to copy one arraylist to another

--- ArrayLists/src/com/company/GroceryList.java
+++ ArrayLists/src/com/company/GroceryList.java
@@ -12,6 +12,10 @@ public class GroceryList {
         groceryList.add(item);
     }
 
+    public ArrayList<String> getGroceryList() {
+        return groceryList;
+    }
+
     // Print list
     public void printGroceryList(){
         System.out.println("You have " + groceryList.size() + " items in your grocery list");
@@ -21,27 +25,34 @@ public class GroceryList {
     }
 
     // Method to modify list less redundantly
-    public void modifyGroceryItem(String newItem){
-        int position = findItem(newItem);
+    public void modifyGroceryItem(String currentItem, String newItem){
+        int position = findItem(currentItem);
         if (position >= 0){
             modifyGroceryItem(position, newItem);
         }
     }
 
     // Replace an item in the list
-    public void modifyGroceryItem(int position, String newItem){
+    private void modifyGroceryItem(int position, String newItem){
         groceryList.set(position, newItem);
         System.out.println("Grocery item " + (position+1) + " has been modified");
     }
 
+    public void removeGroceryItem(String item){
+        int position = findItem(item);
+        if (position >= 0){
+            modifyGroceryItem(position, item);
+        }
+    }
+
     // Remove an item
-    public void removeGroceryItem(int position){
-        String theItem = groceryList.get(position);
+    private void removeGroceryItem(int position){
+//        String theItem = groceryList.get(position);
         groceryList.remove(position);
     }
 
     // Find item returns true or false
-    public int findItem(String searchItem){
+    private int findItem(String searchItem){
 //        boolean exists = groceryList.contains(searchItem);
         // Return item that's actualy in the list
         return groceryList.indexOf(searchItem);
@@ -50,5 +61,14 @@ public class GroceryList {
 //            return groceryList.get(position);
 //        }
 //        return null;
+    }
+
+    // Method to enable someone to pass in the name of an item and return it
+    public boolean onFile(String searchItem){
+        int position = findItem(searchItem);
+        if (position >= 0){
+            return true;
+        }
+        return false;
     }
 }
--- ArrayLists/src/com/company/Main.java
+++ ArrayLists/src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -37,6 +38,9 @@ public class Main {
                     searchForItem();
                     break;
                 case 6:
+                    processArrayList();
+                    break;
+                case 7:
                     quit = true;
                     break;
             }
@@ -64,28 +68,36 @@ public class Main {
 
     // Modify item
     public static void modifyItem(){
-        System.out.print("Enter item number: ");
-        int itemNo = scanner.nextInt();
+        System.out.print("Current item name: ");
+        String itemNo = scanner.nextLine();
         scanner.nextLine();
         System.out.println("Enter replacement item: ");
         String newItem = scanner.nextLine();
-        groceryList.modifyGroceryItem(itemNo - 1, newItem);
+        groceryList.modifyGroceryItem(itemNo, newItem);
     }
 
     public static void removeItem(){
-        System.out.println("Enter item number: ");
-        int itemNo = scanner.nextInt();
+        System.out.println("Enter item name: ");
+        String itemNo = scanner.nextLine();
         scanner.nextLine();
-        groceryList.removeGroceryItem(itemNo - 1);
+        groceryList.removeGroceryItem(itemNo);
     }
 
     public static void searchForItem(){
         System.out.println("Item to search for: ");
         String searchItem = scanner.nextLine();
-        if (groceryList.findItem(searchItem) != null){
+        if (groceryList.onFile(searchItem)){
             System.out.println("Found " + searchItem + " in our grocery list");
         } else {
             System.out.println(searchItem + " is not in our grocery list");
         }
+    }
+
+    // Method to copy one arraylist to another arraylist
+    public static void processArrayList(){
+        ArrayList<String> newArray = new ArrayList<>();
+        newArray.addAll(groceryList.getGroceryList());
+
+        ArrayList<String> newArray2 = new ArrayList<>(groceryList.getGroceryList());
     }
 }
