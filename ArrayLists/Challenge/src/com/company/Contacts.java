bccd42a451d1fee8267d059e3a3e2ad50a84a75e
Yahya Hussein <yahyahussein91@gmail.com>
2020-12-09 22:24:00 -0800

    re doing arraylist challenge

--- ArrayLists/Challenge/src/com/company/Contacts.java
+++ ArrayLists/Challenge/src/com/company/Contacts.java
@@ -1,24 +1,5 @@
 package com.company;
 
 public class Contacts {
-    private String name;
-    private String phoneNumber;
-
-    public Contacts(String name, String phoneNumber) {
-        this.name = name;
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public String getPhoneNumber() {
-        return this.phoneNumber;
-    }
-
-    // Method to create a contact
-    public static Contacts createContact(String name, String phoneNumber){
-        return new Contacts(name, phoneNumber);
-    }
+    
 }
