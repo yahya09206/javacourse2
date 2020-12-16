5efe64894b70f356ac9d7a4ab54081e60d8ffc1e
Yahya Hussein <yahyahussein91@gmail.com>
2020-12-15 20:55:55 -0800

    added methods to add and print contacts

--- ArrayLists/Challenge/src/com/company/MobilePhone.java
+++ ArrayLists/Challenge/src/com/company/MobilePhone.java
@@ -4,5 +4,24 @@ import java.util.ArrayList;
 
 public class MobilePhone {
 
+    private static ArrayList<Contacts> myContacts = new ArrayList<>();
+
+    //Method to print contact list
+    private static void printContacts(){
+        if (myContacts.size() == 0){
+            System.out.println("There are no contacts available");
+        }else{
+            for (int i = 0; i < myContacts.size(); i++){
+                System.out.println("Name: " + myContacts.get(i).getName());
+            }
+        }
+    }
+
+    //Method to add contacts to phone
+    public static void addContact(Contacts contacts){
+        String name = contacts.getName();
+        myContacts.add(contacts);
+        System.out.println("You have added " + name + " to the contact list");
+    }
 
 }
