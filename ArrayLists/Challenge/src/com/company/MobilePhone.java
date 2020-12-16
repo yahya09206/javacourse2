eb1ae88aa3fe7f916c1df04f4de77ef5ee743560
Yahya Hussein <yahyahussein91@gmail.com>
2020-12-15 21:20:49 -0800

    added rest of methods to find remove and edit contact

--- ArrayLists/Challenge/src/com/company/MobilePhone.java
+++ ArrayLists/Challenge/src/com/company/MobilePhone.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MobilePhone {
 
@@ -22,6 +23,50 @@ public class MobilePhone {
         String name = contacts.getName();
         myContacts.add(contacts);
         System.out.println("You have added " + name + " to the contact list");
+    }
+
+    //Method to find contact
+    public static int findContact(String name){
+        int index = -1;
+        for (int i = 0; i < myContacts.size(); i++){
+            if (name.equalsIgnoreCase(myContacts.get(i).getName())){
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    //Method to remove contact
+    public static void removeContact(String name){
+        int index = findContact(name);
+        if (index < 0){
+            System.out.println("Name does not exist in contact list");
+        }else {
+            myContacts.remove(index);
+            System.out.println(name + " has been removed from contact list");
+        }
+    }
+
+    //Method to edit contact
+    public static void editContact(String name){
+        Scanner scanner = new Scanner(System.in);
+        int index = findContact(name);
+        if (index < 0){
+            System.out.println("Name does not exist in the contact list");
+        }else {
+            System.out.print("Enter new name(press Enter to make no changes) to the contact");
+            String newName= scanner.nextLine();
+            if (newName.isEmpty()){
+                newName = name;
+            }
+            System.out.print("Enter new number(press Enter to make no changes) to the contact");
+            String newPhoneNumber = scanner.nextLine();
+            if (newPhoneNumber.isEmpty()){
+                newPhoneNumber = myContacts.get(index).getPhoneNumber();
+            }
+            myContacts.set(index, new Contacts(newName, newPhoneNumber));
+            System.out.println(newName + " has been edited");
+        }
     }
 
 }
